01e1cf51ad65c6c92f8e7739d873a267707ceac6
Komissarts <komissarkomisan@gmail.com>
2022-04-26 17:53:44 +1000

    Fixed Camera Switching

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -4,10 +4,138 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
-    public GameObject Camera1;
-    public GameObject Camera2;
-    public AudioListener CamAuLis1;
-    public AudioListener CamAuLis2;
+    [SerializeField] private Camera[] cameras;
+    private AudioListener[] aud_list;
+
+    //public Camera Camera1;
+    //public GameObject Camera2;
+    //public AudioListener CamAuLis1;
+    //public AudioListener CamAuLis2;
+    private Camera activeCam;
+    private int currCam = 0;
+    private bool laserActive = true;
+
+    //private [SerializeField] KeyCode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.aud_list = new AudioListener[cameras.Length];
+        for(int i = 0; i < cameras.Length; i++)
+        {
+            aud_list[i] = cameras[i].GetComponent<AudioListener>();
+            aud_list[i].enabled = false;
+        }
+        cameras[0].enabled = true;
+        aud_list[0].enabled = true;
+        this.activeCam = cameras[0];
+        //cameraPositionChange(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyCodeCheck();
+        ActivateLaser();
+    }
+
+    private void keyCodeCheck()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            //int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
+            
+            cameraPositionChange();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (laserActive) this.laserActive = false;
+            else { this.laserActive = true; }
+        }
+    }
+
+    private void ActivateLaser()
+    {
+        if (laserActive)
+        {
+            Ray laserRay = activeCam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(laserRay, out RaycastHit raycastHit))
+            {
+                transform.position = raycastHit.point;
+            }
+        }
+    }
+
+    private void cameraPositionChange()
+    {
+        this.currCam++;
+
+        if (this.currCam >= cameras.Length)
+        {
+            this.currCam = 0;
+            //PlayerPrefs.SetInt("CameraPosition", 0);
+        }
+
+        //PlayerPrefs.SetInt("CameraPosition", camPos);
+
+        //if (camPos == 0)
+        //{
+        //    this.activeCam = Camera1.GetComponent<Camera>();
+        //    Camera1.enabled = true;
+        //    CamAuLis1.enabled = true;
+        //
+        //    Camera2.SetActive(false);
+        //    CamAuLis2.enabled = false;
+        //}
+        //
+        //if (camPos == 1)
+        //{
+        //    //activeCam.enabled = false;
+        //    this.activeCam = Camera2.GetComponent<Camera>();
+        //    Camera2.SetActive(true);
+        //    CamAuLis2.enabled = true;
+        //
+        //    Camera1.enabled = false;
+        //    CamAuLis1.enabled = false;
+        //}
+
+        for (int i = 0; i < cameras.Length; i++)
+        {
+            //if(i == camPos) ++i;
+
+            //else
+            //{
+            //if (this.currCam == 0) i--;
+
+                cameras[i].enabled = false;
+                aud_list[i].enabled = false;
+            if (i == this.currCam)
+            {
+                cameras[currCam].enabled = true;
+                aud_list[currCam].enabled = true;
+                this.activeCam = cameras[currCam];
+                //i++;
+            }
+            //}
+
+        }
+        //cameras[camPos].enabled = true;
+        //aud_list[camPos].enabled = true;
+        //this.activeCam = cameras[camPos];
+    }
+}
+
+
+
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject[] Cameras;
+    private AudioListener[] CamAuLis;
     private Camera activeCam;
 
     //private [SerializeField] KeyCode;
@@ -15,6 +143,13 @@ public class CameraSwitch : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        for(int i = 0; i <= Cameras.Length; i++)
+        {
+            this.CamAuLis[i] = Cameras[i].GetComponent<AudioListener>();
+        }
+        Cameras[0].active = true;
+        CamAuLis[0].enabled = true;
         cameraPositionChange(PlayerPrefs.GetInt("CameraPosition"));
     }
 
@@ -33,15 +168,10 @@ public class CameraSwitch : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Q))
         {
-            cameraChangeIterator();
+            int camPosCounter = PlayerPrefs.GetInt("CameraPosition");
+            camPosCounter++;
+            cameraPositionChange(camPosCounter);
         }
-    }
-
-    private void cameraChangeIterator()
-    {
-        int cameraPositionCounter = PlayerPrefs.GetInt("CameraPosition");
-        cameraPositionCounter++;
-        cameraPositionChange(cameraPositionCounter);
     }
 
     private void cameraPositionChange(int camPos)
@@ -49,49 +179,26 @@ public class CameraSwitch : MonoBehaviour
         
 
 
-        if (camPos > 1)
+        if (camPos > Cameras.Length)
         {
             camPos = 0;
         }
 
         PlayerPrefs.SetInt("CameraPosition", camPos);
 
-        if (camPos == 0)
+        Cameras[camPos].active = true;
+        CamAuLis[camPos].enabled = true;
+        this.activeCam = Cameras[camPos].GetComponent<Camera>();
+
+
+        for (int i = 0; i < Cameras.Length; i++)
         {
-            this.activeCam = Camera1.GetComponent<Camera>();
-            Camera1.SetActive(true);
-            CamAuLis1.enabled = true;
-
-            Camera2.SetActive(false);
-            CamAuLis2.enabled = false;
+            if (i == camPos) i++;
+            Cameras[i].active= false;
+            CamAuLis[i].enabled = false;
         }
-
-        if (camPos == 1)
-        {
-            this.activeCam = Camera2.GetComponent<Camera>();
-            Camera2.SetActive(true);
-            CamAuLis2.enabled = true;
-
-            Camera1.SetActive(false);
-            CamAuLis1.enabled = false;
-        }
-
-
-
-        //for (int i = 0; i < Cameras.Length; i++)
-        //{
-        //    if(i != camPos)
-        //    {
-        //        Cameras[camPos].SetActive(false);
-        //        CameraAudioListener[camPos].enabled = false;
-        //    }
-        //    else
-        //    {
-        //        Cameras[i].SetActive(true);
-        //        CameraAudioListener[i].enabled = true;
-        //    }
-        //}
 
         //disableOtherCameras(camPos);
     }
 }
+*/
