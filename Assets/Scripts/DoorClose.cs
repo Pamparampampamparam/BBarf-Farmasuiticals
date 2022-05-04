a53be09af0d6d77f716dde0949ce7681568e6237
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2022-05-04 21:58:54 +1000

    Code&Prefabs

--- Assets/Scripts/DoorClose.cs
+++ Assets/Scripts/DoorClose.cs
@@ -12,11 +12,11 @@ public class DoorClose : MonoBehaviour
     {
         for(int i = 0; i < doors.Length; i++)
         {
-            if (doors[i].transform.position.y >= 5.5f)
+            if (doors[i].transform.position.y >= 20f)
             {
                 isOpened = true;
             }
-            else if (doors[i].transform.position.y <= 1.5f)
+            else if (doors[i].transform.position.y <= 10f)
             {
                 isOpened = false;
             }
@@ -29,14 +29,14 @@ public class DoorClose : MonoBehaviour
         //{
             for (int i = 0; i < doors.Length; i++)
             {
-                if (doors[i].transform.position.y >= 5.5f)
+                if (doors[i].transform.position.y >= 20f)
                 {
                     //isOpened = true;
-                    doors[i].transform.position -= new Vector3(0, 4, 0);
+                    doors[i].transform.position -= new Vector3(0, 10, 0);
                 }
-                else if(doors[i].transform.position.y <= 1.5f) 
+                else if(doors[i].transform.position.y <= 10f) 
                 {
-                    Vector3 downPosition = new Vector3(doors[i].transform.position.x, 1.5f, doors[i].transform.position.z);
+                    Vector3 downPosition = new Vector3(doors[i].transform.position.x, 10f, doors[i].transform.position.z);
                     doors[i].transform.position = downPosition;
                 }
                 //else
@@ -55,14 +55,14 @@ public class DoorClose : MonoBehaviour
         //{
             for (int i = 0; i < doors.Length; i++)
             {
-                if (doors[i].transform.position.y >= 5.5f)
+                if (doors[i].transform.position.y >= 20f)
                 {
-                Vector3 upPosition = new Vector3(doors[i].transform.position.x, 5.5f, doors[i].transform.position.z);
+                Vector3 upPosition = new Vector3(doors[i].transform.position.x, 20f, doors[i].transform.position.z);
                 doors[i].transform.position = upPosition;
             }
-                else if (doors[i].transform.position.y <= 1.5f)
+                else if (doors[i].transform.position.y <= 10f)
                 {
-                    doors[i].transform.position += new Vector3(0, 4, 0);
+                    doors[i].transform.position += new Vector3(0, 10, 0);
                 }
 
                 //isOpened = false;
--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -15,15 +15,15 @@ public class DoorOpen : MonoBehaviour
         {
             for(int i = 0; i < doors.Length; i++)
             {
-                if (doors[i].transform.position.y >= 5.5f)
+                if (doors[i].transform.position.y >= 20f)
                 {
-                    Vector3 upPosition = new Vector3(doors[i].transform.position.x, 5.5f, doors[i].transform.position.z);
+                    Vector3 upPosition = new Vector3(doors[i].transform.position.x, 20f, doors[i].transform.position.z);
                     doors[i].transform.position = upPosition;
                 }
-                else if (doors[i].transform.position.y <= 1.5f)
+                else if (doors[i].transform.position.y <= 10f)
                 {
                     isOpened = true;
-                    doors[i].transform.position += new Vector3(0, 4, 0);
+                    doors[i].transform.position += new Vector3(0, 10, 0);
                 }
                 //else
                 //{
@@ -45,13 +45,13 @@ public class DoorOpen : MonoBehaviour
         //{
             for (int i = 0; i < doors.Length; i++)
             {
-                if (doors[i].transform.position.y >= 5.5f)
+                if (doors[i].transform.position.y >= 20f)
                 {
-                    doors[i].transform.position -= new Vector3(0, 4, 0);
+                    doors[i].transform.position -= new Vector3(0, 10, 0);
                 }
-                else if (doors[i].transform.position.y <= 1.5f)
+                else if (doors[i].transform.position.y <= 10f)
                 {
-                    Vector3 downPosition = new Vector3(doors[i].transform.position.x, 1.5f, doors[i].transform.position.z);
+                    Vector3 downPosition = new Vector3(doors[i].transform.position.x, 10f, doors[i].transform.position.z);
                     doors[i].transform.position = downPosition;
                 }
                 //isOpened = false;
