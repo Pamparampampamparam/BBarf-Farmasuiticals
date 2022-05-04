2b1a9e8fdbb6a8d0b84bed027add271d603967b8
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2022-05-04 22:35:05 +1000

    FixedPlateScripts

--- Assets/Scripts/DoorOpen.cs
+++ Assets/Scripts/DoorOpen.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
-    //[SerializeField] GameObject door;
     [SerializeField] GameObject[] doors;
-
-    bool isOpened = false;
 
     private void OnTriggerEnter(Collider col)
     {
-        if(isOpened == false)
-        {
             for(int i = 0; i < doors.Length; i++)
             {
                 if (doors[i].transform.position.y >= 20f)
@@ -22,27 +17,13 @@ public class DoorOpen : MonoBehaviour
                 }
                 else if (doors[i].transform.position.y <= 10f)
                 {
-                    isOpened = true;
                     doors[i].transform.position += new Vector3(0, 10, 0);
                 }
-                //else
-                //{
-                    //isOpened = true;
-                    //doors[i].transform.position += new Vector3(0, 4, 0);
-                //}
             }
-        }
-        //if(isOpened == false)
-        //{
-            //isOpened = true;
-            //door.transform.position += new Vector3(0, 4, 0);
-        //}
     }
 
     private void OnTriggerExit(Collider col)
     {
-        //if (isOpened == true)
-        //{
             for (int i = 0; i < doors.Length; i++)
             {
                 if (doors[i].transform.position.y >= 20f)
@@ -54,14 +35,6 @@ public class DoorOpen : MonoBehaviour
                     Vector3 downPosition = new Vector3(doors[i].transform.position.x, 10f, doors[i].transform.position.z);
                     doors[i].transform.position = downPosition;
                 }
-                //isOpened = false;
-                //doors[i].transform.position -= new Vector3(0, 4, 0);
             }
-        //}
-        //if (isOpened == true)
-        //{
-            //isOpened = false;
-           // door.transform.position -= new Vector3(0, 4, 0);
-        //}
     }
 }
