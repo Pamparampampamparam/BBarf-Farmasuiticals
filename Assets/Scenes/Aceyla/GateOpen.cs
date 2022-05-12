ddd2caccc077eb0cce2fe4854ca316736f93a9ba
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2022-05-12 17:32:02 +1000

    FixedPuzzle:)

--- Assets/Scenes/Aceyla/GateOpen.cs
+++ Assets/Scenes/Aceyla/GateOpen.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class GateOpen : MonoBehaviour
 {
     [SerializeField] GameObject[] doors;
+    [SerializeField] GameObject[] indicator;
+    Color red = new Color(200, 0, 0, 1);
+    Color green = new Color(0, 200, 0, 1);
 
     private void OnTriggerEnter(Collider col)
     {
             for (int i = 0; i < doors.Length; i++)
             {
-                if (doors[i].transform.position.y >= 34f)
+                if (doors[i].transform.position.y >= 20f)
                 {
-                    Vector3 upPosition = new Vector3(doors[i].transform.position.x, 34f, doors[i].transform.position.z);
+                    Vector3 upPosition = new Vector3(doors[i].transform.position.x, 20f, doors[i].transform.position.z);
                     doors[i].transform.position = upPosition;
+                    indicator[i].GetComponent<Renderer>().material.SetColor("_Color", green);
                 }
-                else if (doors[i].transform.position.y <= 24f)
+                else if (doors[i].transform.position.y <= 10f)
                 {
                     doors[i].transform.position += new Vector3(0, 10, 0);
+                    indicator[i].GetComponent<Renderer>().material.SetColor("_Color", green);
                 }
             }
     }
@@ -26,14 +31,16 @@ public class GateOpen : MonoBehaviour
     {
         for (int i = 0; i < doors.Length; i++)
         {
-            if (doors[i].transform.position.y >= 34f)
+            if (doors[i].transform.position.y >= 20f)
             {
                 doors[i].transform.position -= new Vector3(0, 10, 0);
+                indicator[i].GetComponent<Renderer>().material.SetColor("_Color", red);
             }
-            else if (doors[i].transform.position.y <= 24f)
+            else if (doors[i].transform.position.y <= 10f)
             {
-                Vector3 downPosition = new Vector3(doors[i].transform.position.x, 24f, doors[i].transform.position.z);
+                Vector3 downPosition = new Vector3(doors[i].transform.position.x, 10f, doors[i].transform.position.z);
                 doors[i].transform.position = downPosition;
+                indicator[i].GetComponent<Renderer>().material.SetColor("_Color", red);
             }
         }
     }
