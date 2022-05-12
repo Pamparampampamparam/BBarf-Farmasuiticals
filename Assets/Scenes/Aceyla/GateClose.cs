3481bd44cb1a02a552fc118230d186fd214e2a15
Acelya1907 <103116528+TAMBIZIBIDI@users.noreply.github.com>
2022-05-12 15:54:48 +1000

    FixedCode

--- Assets/Scenes/Aceyla/GateClose.cs
+++ Assets/Scenes/Aceyla/GateClose.cs
@@ -6,32 +6,12 @@ public class GateClose : MonoBehaviour
 {
     [SerializeField] GameObject[] doors; //FIIIIIXXXXX
 
-    //bool isOpened = false;
-
-    //void Update()
-    //{
-        //for (int i = 0; i < doors.Length; i++)
-        //{
-            //if (doors[i].transform.position.y >= 5.5f)
-            //{
-                //isOpened = true;
-            //}
-            //else if (doors[i].transform.position.y <= 1.5f)
-            //{
-                //isOpened = false;
-            //}
-        //}
-    //}
-
     private void OnTriggerEnter(Collider col)
     {
-        //if (isOpened == false)
-        //{
         for (int i = 0; i < doors.Length; i++)
         {
             if (doors[i].transform.position.y >= 34f)
             {
-                //isOpened = true;
                 doors[i].transform.position -= new Vector3(0, 10, 0);
             }
             else if (doors[i].transform.position.y <= 24f)
@@ -39,35 +19,24 @@ public class GateClose : MonoBehaviour
                 Vector3 downPosition = new Vector3(doors[i].transform.position.x, 24f, doors[i].transform.position.z);
                 doors[i].transform.position = downPosition;
             }
-            //else
-            //{
-            //isOpened = true;
-            //doors[i].transform.position -= new Vector3(0, 4, 0);
-            //}
-
         }
-        //}
     }
 
     private void OnTriggerExit(Collider col)
     {
-        //if (isOpened == true)
-        //{
         for (int i = 0; i < doors.Length; i++)
         {
             if (doors[i].transform.position.y >= 34f)
             {
+                doors[i].transform.position -= new Vector3(0, 10, 0);
                 Vector3 upPosition = new Vector3(doors[i].transform.position.x, 34f, doors[i].transform.position.z);
                 doors[i].transform.position = upPosition;
             }
             else if (doors[i].transform.position.y <= 24f)
             {
-                doors[i].transform.position += new Vector3(0, 10, 0);
+                Vector3 downPosition = new Vector3(doors[i].transform.position.x, 20f, doors[i].transform.position.z);
+                doors[i].transform.position = downPosition;
             }
-
-            //isOpened = false;
-            //doors[i].transform.position += new Vector3(0, 4, 0);
         }
-        //}
     }
 }
--- Assets/Scenes/Aceyla/GateOpen.cs
+++ Assets/Scenes/Aceyla/GateOpen.cs
@@ -6,12 +6,8 @@ public class GateOpen : MonoBehaviour
 {
     [SerializeField] GameObject[] doors;
 
-    //bool isOpened = false;
-
     private void OnTriggerEnter(Collider col)
     {
-        //if (isOpened == false)
-        //{
             for (int i = 0; i < doors.Length; i++)
             {
                 if (doors[i].transform.position.y >= 34f)
@@ -21,27 +17,13 @@ public class GateOpen : MonoBehaviour
                 }
                 else if (doors[i].transform.position.y <= 24f)
                 {
-                    //isOpened = true;
                     doors[i].transform.position += new Vector3(0, 10, 0);
                 }
-                //else
-                //{
-                //isOpened = true;
-                //doors[i].transform.position += new Vector3(0, 4, 0);
-                //}
             }
-        //}
-        //if(isOpened == false)
-        //{
-        //isOpened = true;
-        //door.transform.position += new Vector3(0, 4, 0);
-        //}
     }
 
     private void OnTriggerExit(Collider col)
     {
-        //if (isOpened == true)
-        //{
         for (int i = 0; i < doors.Length; i++)
         {
             if (doors[i].transform.position.y >= 34f)
@@ -53,14 +35,6 @@ public class GateOpen : MonoBehaviour
                 Vector3 downPosition = new Vector3(doors[i].transform.position.x, 24f, doors[i].transform.position.z);
                 doors[i].transform.position = downPosition;
             }
-            //isOpened = false;
-            //doors[i].transform.position -= new Vector3(0, 4, 0);
         }
-        //}
-        //if (isOpened == true)
-        //{
-        //isOpened = false;
-        // door.transform.position -= new Vector3(0, 4, 0);
-        //}
     }
 }
