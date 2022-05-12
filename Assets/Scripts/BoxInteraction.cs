987385ccae9936b24d4c645b57e06c6336162ca6
emmalum <emmalum20@gmail.com>
2022-05-12 12:29:58 +1000

    Revert "stuff"
    This reverts commit edc8a928bdc47dcb2ae87b72d5edf985aa4d0726, reversing
    changes made to 0a6cc48f077a4df4f0b9b80358d5f2cec0af74c0.

--- Assets/Scripts/BoxInteraction.cs
+++ Assets/Scripts/BoxInteraction.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class BoxInteraction : MonoBehaviour
 {
-    //    [SerializeField]
-    //    private GameObject cat;
-    //    //bool hidden = false;
-    //    private Vector3 spawnpos = new Vector3 (0.0f, 0.0f, 1f);
-    //    //private Quaternion rotation;
-    //    [SerializeField] List<GameObject> inBoxes = new List<GameObject> ();
-    //    [SerializeField] List<GameObject> outBoxes = new List<GameObject>();
-    [SerializeField] Vector3 spawnPointOffset = new Vector3(0.0f, 0.0f, 1f);
-    public bool isInputOnly;
-    public bool isOutputOnly;
-    [SerializeField] GameObject ConnectedBox;
+    [SerializeField]
+    private GameObject cat;
+    //bool hidden = false;
+    private Vector3 spawnpos = new Vector3 (0.0f, 0.0f, 1f);
+    //private Quaternion rotation;
+    [SerializeField] List<GameObject> inBoxes = new List<GameObject> ();
+    [SerializeField] List<GameObject> outBoxes = new List<GameObject>();
 
     void Update()
     {
@@ -22,23 +18,23 @@ public class BoxInteraction : MonoBehaviour
             ComeOut();*/
     }
 
-//    private void OnCollisionEnter(Collision collision)
-//    {
-//        GameObject gameObject = collision.gameObject;
-//        ContactPoint contact = collision.contacts[0];
-//        //spawnpos = contact.point;
-//        //rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
-//        if (gameObject.tag == "In_Box")
-//        {
-//            int index = inBoxes.IndexOf(gameObject);
-//            //hidden = true;
-//            Vector3 boxPos = outBoxes[index].transform.position;
-//            Debug.Log(boxPos);
-//            cat.transform.position = boxPos + spawnpos;
-//            Debug.Log(cat.transform.position);
-//            //Debug.Log(hidden);
-//        }
-//    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject gameObject = collision.gameObject;
+        ContactPoint contact = collision.contacts[0];
+        //spawnpos = contact.point;
+        //rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        if (gameObject.tag == "In_Box")
+        {
+            int index = inBoxes.IndexOf(gameObject);
+            //hidden = true;
+            Vector3 boxPos = outBoxes[index].transform.position;
+            Debug.Log(boxPos);
+            cat.transform.position = boxPos + spawnpos;
+            Debug.Log(cat.transform.position);
+            //Debug.Log(hidden);
+        }
+    }
 
     /*void ComeOut()
     {
@@ -50,30 +46,4 @@ public class BoxInteraction : MonoBehaviour
             Debug.Log("Coming out!");
         }
     }*/
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        GameObject Cat = collision.gameObject;
-        //ContactPoint contact = collision.contacts[0];
-
-        if(Cat.tag == "Repel")
-        {
-            if (isInputOnly && ConnectedBox.GetComponent<BoxInteraction>().isOutputOnly)
-            {
-                Cat.transform.position = ConnectedBox.transform.position + spawnPointOffset;
-            }
-        }
-        ////spawnpos = contact.point;
-        ////rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        //if (gameObject.tag == "In_Box")
-        //{
-        //    int index = inBoxes.IndexOf(gameObject);
-        //    //hidden = true;
-        //    Vector3 boxPos = outBoxes[index].transform.position;
-        //    //Debug.Log(boxPos);
-        //    cat.transform.position = boxPos + spawnpos;
-        //    //Debug.Log(cat.transform.position);
-        //    //Debug.Log(hidden);
-        //}
-    }
 }
